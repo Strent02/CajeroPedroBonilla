ec38345b381812b401a0b40abc51b7684306de89
Pedro Bonilla <Pedro Bonilla@DESKTOP-CC1UL3F>
2025-09-29 02:06:20 -0500

    Agregar archivos de proyecto.

---
+++ CajeroPedroBonilla/Objetos/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroPedroBonilla.Objetos
+{
+    internal class Usuario
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Hola, soy un usuario del cajero automático.");
+        }
+    }
+}
---
+++ CajeroPedroBonilla/Program.cs
@@ -0,0 +1,19 @@
+﻿using CajeroPedroBonilla.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroPedroBonilla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            Usuario usuario = new Usuario();
+            usuario.MostrarMensaje();
+        }
+    }
+}
